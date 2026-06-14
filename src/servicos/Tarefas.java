51e7802e31de829705293165ba1b562bbf4c357d
Edwin Machuza <152198197+Usarmame@users.noreply.github.com>
2026-06-14 22:02:51 +0200

    Adicionar LimparTela nas Tarefas

--- src/servicos/Tarefas.java
+++ src/servicos/Tarefas.java
@@ -11,10 +11,11 @@ public interface Tarefas {
     public Resposta adicionarCategoria();
     public Resposta adicionarProdutosEstabelecimento(String idEstabelecimento, String idArmazem, String produto);
     public void adicionarProdutosArmazem(String id);
-        public Resposta registrarVenda(String idEstabelecimento, String idArmazem);
+    public Resposta registrarVenda(String idEstabelecimento, String idArmazem);
     public Gerente getDados();
     public void listarCategorias();
     public void listarVendas(String idEstabelecimento);
     public void listarProdutos(String id, boolean estabelecimento);
-
+    public void limparTela();
+    
 }
